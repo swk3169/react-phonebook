889b4f16ced7cc3f759f5ad42bfcf1b8ad71818d
swk3169 <swk3169@gmail.com>
2018-07-24 05:28:14 +0900

    update-ver0.1
    crud 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
+// file: src/App.js
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '손우규',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data})
+    })
+  }
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+          ? { ...info, ...data}  // 새 객체를 만들어서 기존의 값과 전달받은 data를 덮어씀
+          : info  // 기존의 값을 그대로 유지
+      )
+    })
+  }
   render() {
+    const { information } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <PhoneForm 
+          onCreate={this.handleCreate}
+        />
+        <PhoneInfoList 
+          data={this.state.information}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
       </div>
     );
   }
---
+++ src/components/PhoneInfo.js
@@ -0,0 +1,120 @@
+// file: src/components/PhoneInfo.js
+import React, { Component } from 'react';
+
+class PhoneInfo extends Component {
+    static defaultProps = {
+        info: {
+            name: '이름',
+            phone: '010-0000-0000',
+            id: 0
+        },
+    }
+
+    state = {
+        // 수정 버튼을 눌렀을 때 editing 값을 true로 설정해야한다.
+        // 이 값이 true일 때에는, 기존에 텍스트 형태로 보여주던 값들을
+        // input 형태로 보여주게 된다.
+        editing: false,
+        // input의 값은 유동적이다.
+        // input 값을 담기 위해서 각 필드를 위한 값을 설정
+        name: '',
+        phone: '',
+    }
+
+    handleRemove = () => {
+        // 삭제 버튼이 클릭되면 onRemove에 id를 넣어서 호출
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+
+    // editing 값을 반전시키는 함수
+    // true -> false, false -> true
+    handleToggleEdit = () => {
+        const { editing } = this.state;
+        this.setState({ editing: !editing });
+    }
+
+    // input에서 onChange 이벤트가 발생 될 때 호출되는 함수
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // editing 값이 바뀔 때 처리할 로직이 적혀있다.
+        // 수정을 눌렀을 땐, 기존의 값이 input에 나타나고,
+        // 수정을 적용할 땐, input의 값들을 부모한테 전달해준다.
+
+        const { info, onUpdate } = this.props;
+        if (!prevState.editing && this.state.editing) {
+            // editing 값이 false -> true로 전환 될 때
+            // info의 값을 state에 넣어준다.
+            this.setState({
+                name: info.name,
+                phone: info.phone
+            })
+        }
+
+        if (prevState.editing && !this.state.editing) {
+            // editing 값이 true -> false로 전환 될 때
+            onUpdate(info.id, {
+                name: this.state.name,
+                phone: this.state.phone
+            });
+        }
+    }
+
+    render() {
+        const style = {
+            border: '1px solid black',
+            padding: '8px',
+            margin: '8px'
+        };
+
+        const { editing } = this.state;
+
+        // 수정모드
+        if (editing) {
+            return (
+                <div style={style}>
+                    <div>
+                        <input
+                            value={this.state.name}
+                            name="name"
+                            placeholder="이름"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            value={this.state.phone}
+                            name="phone"
+                            placeholder="전화번호"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <button onClick={this.handleToggleEdit}>적용</button>
+                    <button onClick={this.handleRemove}>삭제</button>
+                </div>
+            );
+        }
+
+        // 일반모드
+        const {
+            name, phone
+        } = this.props.info;
+
+        return (
+            <div style={style}>
+                <div><b>{name}</b></div>
+                <div>{phone}</div>
+                <button onClick={this.handleToggleEdit}>수정</button>
+                <button onClick={this.handleRemove}>삭제</button>
+            </div>
+        );
+    }
+}
+
+export default PhoneInfo;
